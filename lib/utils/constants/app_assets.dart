a1faa57b688605518959d11b15b93f6b61aca1c9
Sadig Aliyev <aliyev.sdg@gmail.com>
2024-12-17 23:05:34 +0400

    - splash_screen.dart is done

---
+++ lib/utils/constants/app_assets.dart
@@ -0,0 +1,6 @@
+class AppAssets {
+  AppAssets._();
+  static const splashBurger1 = "assets/png/splash_burger_1.png";
+  static const splashBurger2 = "assets/png/splash_burger_2.png";
+  static const foodgoLogo = "assets/svg/foodgo_logo.svg";
+}
---
+++ lib/utils/constants/app_colors.dart
@@ -0,0 +1,7 @@
+import 'dart:ui';
+
+class AppColors{
+  AppColors._();
+
+  static const mainRed = Color(0xffEF2A39);
+}
