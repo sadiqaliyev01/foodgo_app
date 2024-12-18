d07cea282cd95230af6421f762d49041f697bc9f
HNihad0 <153919479+HNihad0@users.noreply.github.com>
2024-12-18 21:22:24 +0400

    - resolved conflicts

--- lib/app.dart
+++ lib/app.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:foodgo_app/presentation/screens/order_screen/order_screen.dart';
-import 'package:foodgo_app/presentation/screens/splash_screen/splash_screen.dart';
-import 'package:foodgo_app/utils/constants/app_colors.dart';
+
+import 'presentation/screens/splash_screen/splash_screen.dart';
+import 'utils/constants/app_colors.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -17,7 +17,6 @@ class MyApp extends StatelessWidget {
         scaffoldBackgroundColor: AppColors.white
       ),
       home: const SplashScreen(),
-      debugShowCheckedModeBanner: false,
     );
   }
 }
